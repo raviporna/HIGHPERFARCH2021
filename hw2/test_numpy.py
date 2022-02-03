d1a8f1c4ae31d9b00549f6b0cb7135428c9a3a5f
raviporna <sci09@hotmail.com>
2022-02-03 15:29:45 +0700

    Update test_numpy.py

--- hw2/test_numpy.py
+++ hw2/test_numpy.py
@@ -1,28 +1,33 @@
 import numpy as np
+# import cupy as np
+
 import timeit
 
 size = 6400000
+sampling = 10
 
 # with numpy
 start = timeit.default_timer()
-na = np.random.randint(1, 1000, size)
-nb = np.random.randint(1, 1000, size)
-na += nb
+for i in range(sampling):
+    na = np.random.randint(1, 1000, size)
+    nb = np.random.randint(1, 1000, size)
+    na += nb
 stop = timeit.default_timer()
-t1 = stop-start
+t1 = (stop-start) / sampling
 
 print('Time with numpy: ', t1)
 
 # without numpy
 start = timeit.default_timer()
-a = list(range(size))
-b = list(range(size))
-for i in range(size):
-    a[i] += b[i]
+for i in range(sampling):
+    a = list(range(size))
+    b = list(range(size))
+    for i in range(size):
+        a[i] += b[i]
 stop = timeit.default_timer()
-t2 = stop-start
+t2 = (stop-start) / sampling
 
 print('Time without numpy: ', t2)
 
 speedup = t2/t1
-print('Speedup: ',speedup)
+print('Speedup: ', speedup)
